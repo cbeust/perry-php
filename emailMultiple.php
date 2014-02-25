6f767c4fe7795fb843ddea41d957fb50bd061843
Cedric Beust <cedric@beust.com>
2014-02-25 09:52:52 -0800

    Not too many files

--- emailMultiple.php
+++ emailMultiple.php
@@ -5,11 +5,14 @@ include_once "_common.php";
 $start = $_GET["start"];
 $count = $_GET["count"];
 
+$count = 3;
+
 for ($i = $start; $i < $start + $count; $i++) {
   $englishFile = cbGetEnglishFiles($i);
   $title = $i . ' - ' . cbFindGermanTitle($i);
   echo "Sending " . $i . " Title:" . $title . "<p>\n";
   cbSendEmailWithAttachment($englishFile[0], $title);
+  sleep(10);
 }
 
 ?>
